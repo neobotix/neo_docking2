1508cf88c854a8e4f09a84f4079428bb1bf76599
Pradheep <padhupradheep@gmail.com>
2023-05-03 15:42:17 +0200

    adapting contour matching for undocking

--- src/neo_docking.cpp
+++ src/neo_docking.cpp
@@ -510,10 +510,12 @@ private:
     RCLCPP_INFO(client_node_->get_logger(), "Undocking finished");
 
     // Restarting Contour matching
-    geometry_msgs::msg::Pose init_pose_;
-    contour_matching->setInitialGuess(init_pose_);
-    contour_matching->startMatching();
-
+    if (auto_detect_) {
+      geometry_msgs::msg::Pose init_pose_;
+      contour_matching->setInitialGuess(init_pose_);
+      contour_matching->startMatching();
+    }
+    
     return true;
   }
 
