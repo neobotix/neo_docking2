93da403953fd944f2e3e36bf2c6b61994a4b0c91
Pradheep <padhupradheep@gmail.com>
2023-05-16 17:51:20 +0200

    adding docking params

--- launch/docking_launch.py
+++ launch/docking_launch.py
@@ -8,7 +8,7 @@ from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 
 def generate_launch_description():
-    config = os.path.join(get_package_share_directory('neo_docking2'),'launch','dock_pose.yaml')
+    config = os.path.join(get_package_share_directory('neo_docking2'),'launch','dock_param.yaml')
     return launch.LaunchDescription([
         launch_ros.actions.Node(
             package='neo_docking2', executable='neo_docking2', output='screen', parameters = [config])
