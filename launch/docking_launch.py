3e98782fe0c686c512f1ac688bf8ba58e88f195e
PRP <padhupradheep@gmail.com>
2023-07-13 16:37:33 +0200

    namespace support for manual docking

--- launch/docking_launch.py
+++ launch/docking_launch.py
@@ -6,10 +6,17 @@ import launch.substitutions
 import os
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+    robot_namespace = launch.substitutions.LaunchConfiguration('namespace', default="")
     config = os.path.join(get_package_share_directory('neo_docking2'),'launch','dock_pose.yaml')
+    
     return launch.LaunchDescription([
         launch_ros.actions.Node(
-            package='neo_docking2', executable='neo_docking2', output='screen', parameters = [config])
+            package='neo_docking2',
+            executable='neo_docking2',
+            namespace=robot_namespace,
+            output='screen',
+            parameters = [config])
     ])
