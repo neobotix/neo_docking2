869253b9efce016553a056a827302534ed3116aa
Pradheep <padhupradheep@gmail.com>
2026-08-13 14:03:04 +0200

    updating the timeout for the approaching

--- src/neo_docking.cpp
+++ src/neo_docking.cpp
@@ -342,7 +342,7 @@ public:
         set_none_ = false;
         auto lapsed_time = (this->get_clock()->now() - set_approach_time).seconds();
         if (lapsed_time > 3.0) {
-          if (remaining_distance > 0.01 && lapsed_time < 12.0) {
+          if (remaining_distance > 0.01 && lapsed_time < 14.0) {
             constexpr double docking_speed = 0.05;
             constexpr double slowdown_distance = 0.045;
             constexpr double stop_distance = 0.01;
